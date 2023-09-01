05eabeb74aaaadb69d511b914fe956a6c4f3fe23
Tuxiboule <140740228+Tuxiboule@users.noreply.github.com>
2023-09-01 14:50:45 +0200

    Delete test.py
    Delete useless file

--- test.py
+++
@@ -1,9 +0,0 @@
-from colorama import init, Fore, Style
-
-# Initialisez colorama
-init(autoreset=True)
-
-texte = "Texte à souligner"
-texte_souligne = f"{Style.BRIGHT}{texte}{Style.RESET_ALL}{Style.UNDERLINE}"
-
-print(texte_souligne)
